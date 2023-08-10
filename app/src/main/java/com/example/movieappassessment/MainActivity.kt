6876911e0feb835918df640992ecade7358596c7
Alex Rosa <alexr@gec.inatel.br>
2023-08-10 20:49:54 -0300

    added layouts, recyclerview and adapter to list popular movies

--- app/src/main/java/com/example/movieappassessment/MainActivity.kt
+++ app/src/main/java/com/example/movieappassessment/MainActivity.kt
@@ -4,23 +4,38 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.movieappassessment.adapter.MoviesAdapter
 import com.example.movieappassessment.dataObj.Movie
 import com.example.movieappassessment.repository.MoviesRepository
 import com.google.android.material.color.utilities.MaterialDynamicColors.onError
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var popularMovies: RecyclerView
+    private lateinit var popularMoviesAdapter: MoviesAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        popularMovies = findViewById(R.id.popular_movies)
+        popularMovies.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+
+        popularMoviesAdapter = MoviesAdapter(listOf())
+        popularMovies.adapter = popularMoviesAdapter
 
         MoviesRepository.getPopularMovies(
             onSuccess = ::onPopularMoviesFetched,
             onError = ::onError
         )
+
     }
 
     private fun onPopularMoviesFetched(movies: List<Movie>) {
         Log.d("MainActivity", "Movies: $movies")
+        popularMoviesAdapter.updateMovies(movies)
     }
 
     private fun onError() {
---
+++ app/src/main/java/com/example/movieappassessment/adapter/MoviesAdapter.kt
@@ -0,0 +1,46 @@
+package com.example.movieappassessment.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.CenterCrop
+import com.example.movieappassessment.R
+import com.example.movieappassessment.dataObj.Movie
+
+class MoviesAdapter(
+    private var movies: List<Movie>
+) : RecyclerView.Adapter<MoviesAdapter.MovieViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieViewHolder {
+        val view = LayoutInflater
+            .from(parent.context)
+            .inflate(R.layout.item_movie, parent, false)
+        return MovieViewHolder(view)
+    }
+
+    override fun getItemCount(): Int = movies.size
+
+    override fun onBindViewHolder(holder: MovieViewHolder, position: Int) {
+        holder.bind(movies[position])
+    }
+
+    fun updateMovies(movies: List<Movie>) {
+        this.movies = movies
+        notifyDataSetChanged()
+    }
+
+    inner class MovieViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        private val poster: ImageView = itemView.findViewById(R.id.item_movie_poster)
+
+        fun bind(movie: Movie) {
+            Glide.with(itemView)//TODO https://image.tmdb.org/t/p/w342 to IMAGE_BASE_URL - choose quality to load
+                .load("https://image.tmdb.org/t/p/w342${movie.posterPath}")
+                .transform(CenterCrop())
+                .into(poster)
+        }
+    }
+}
