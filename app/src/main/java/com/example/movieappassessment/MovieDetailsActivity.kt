04d0cd15858f76dddc272d21aeb7d7202a05a2d2
Alex Rosa <alexr@gec.inatel.br>
2023-08-11 10:18:02 -0300

    added view binding to details activity

--- app/src/main/java/com/example/movieappassessment/MovieDetailsActivity.kt
+++ app/src/main/java/com/example/movieappassessment/MovieDetailsActivity.kt
@@ -1,13 +1,10 @@
 package com.example.movieappassessment
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ImageView
-import android.widget.RatingBar
-import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.CenterCrop
+import com.example.movieappassessment.databinding.ActivityMovieDetailsBinding
 
 const val MOVIE_BACKDROP = "extra_movie_backdrop"
 const val MOVIE_POSTER = "extra_movie_poster"
@@ -17,24 +14,13 @@ const val MOVIE_RELEASE_DATE = "extra_movie_release_date"
 const val MOVIE_OVERVIEW = "extra_movie_overview"
 
 class MovieDetailsActivity : AppCompatActivity() {
-
-    private lateinit var backdrop: ImageView
-    private lateinit var poster: ImageView
-    private lateinit var title: TextView
-    private lateinit var rating: RatingBar
-    private lateinit var releaseDate: TextView
-    private lateinit var overview: TextView
+    private lateinit var binding: ActivityMovieDetailsBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_movie_details)
-
-        backdrop = findViewById(R.id.movie_backdrop)
-        poster = findViewById(R.id.movie_poster)
-        title = findViewById(R.id.movie_title)
-        rating = findViewById(R.id.movie_rating)
-        releaseDate = findViewById(R.id.movie_release_date)
-        overview = findViewById(R.id.movie_overview)
+        binding = ActivityMovieDetailsBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
         val extras = intent.extras
 
@@ -50,20 +36,20 @@ class MovieDetailsActivity : AppCompatActivity() {
             Glide.with(this)
                 .load("https://image.tmdb.org/t/p/w1280$backdropPath")
                 .transform(CenterCrop())
-                .into(backdrop)
+                .into(binding.movieBackdrop)
         }
 
         extras.getString(MOVIE_POSTER)?.let { posterPath ->
             Glide.with(this)
                 .load("https://image.tmdb.org/t/p/w342$posterPath")
                 .transform(CenterCrop())
-                .into(poster)
+                .into(binding.moviePoster)
         }
 
-        title.text = extras.getString(MOVIE_TITLE, "")
-        rating.rating = extras.getFloat(MOVIE_RATING, 0f) / 2
-        releaseDate.text = extras.getString(MOVIE_RELEASE_DATE, "")
-        overview.text = extras.getString(MOVIE_OVERVIEW, "")
+        binding.movieTitle.text = extras.getString(MOVIE_TITLE, "")
+        binding.movieRating.rating = extras.getFloat(MOVIE_RATING, 0f) / 2
+        binding.movieReleaseDate.text = extras.getString(MOVIE_RELEASE_DATE, "")
+        binding.movieOverview.text = extras.getString(MOVIE_OVERVIEW, "")
     }
 
 }
