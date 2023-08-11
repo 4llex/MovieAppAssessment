9d90cf62ab16f344672bdedf0406da9a2036413c
Alex Rosa <alexr@gec.inatel.br>
2023-08-10 21:37:16 -0300

    added pagination to show more movies on scrolling

--- app/src/main/java/com/example/movieappassessment/MainActivity.kt
+++ app/src/main/java/com/example/movieappassessment/MainActivity.kt
@@ -9,33 +9,58 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.movieappassessment.adapter.MoviesAdapter
 import com.example.movieappassessment.dataObj.Movie
 import com.example.movieappassessment.repository.MoviesRepository
-import com.google.android.material.color.utilities.MaterialDynamicColors.onError
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var popularMovies: RecyclerView
     private lateinit var popularMoviesAdapter: MoviesAdapter
+    private lateinit var popularMoviesLayoutMgr: LinearLayoutManager
+
+    private var popularMoviesPage = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         popularMovies = findViewById(R.id.popular_movies)
-        popularMovies.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+        popularMoviesLayoutMgr = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
 
-        popularMoviesAdapter = MoviesAdapter(listOf())
+        popularMovies.layoutManager = popularMoviesLayoutMgr
+        popularMoviesAdapter = MoviesAdapter(mutableListOf())
         popularMovies.adapter = popularMoviesAdapter
 
-        MoviesRepository.getPopularMovies(
-            onSuccess = ::onPopularMoviesFetched,
-            onError = ::onError
-        )
+        getPopularMovies()
 
+    }
+
+    private fun attachPopularMoviesOnScrollListener() {
+        popularMovies.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                val totalItemCount = popularMoviesLayoutMgr.itemCount
+                val visibleItemCount = popularMoviesLayoutMgr.childCount
+                val firstVisibleItem = popularMoviesLayoutMgr.findFirstVisibleItemPosition()
+
+                if (firstVisibleItem + visibleItemCount >= totalItemCount / 2) {
+                    popularMovies.removeOnScrollListener(this)
+                    popularMoviesPage++
+                    getPopularMovies()
+                }
+            }
+        })
+    }
+
+    private fun getPopularMovies() {
+        MoviesRepository.getPopularMovies(
+            popularMoviesPage,
+            ::onPopularMoviesFetched,
+            ::onError
+        )
     }
 
     private fun onPopularMoviesFetched(movies: List<Movie>) {
         Log.d("MainActivity", "Movies: $movies")
-        popularMoviesAdapter.updateMovies(movies)
+        popularMoviesAdapter.appendMovies(movies)
+        attachPopularMoviesOnScrollListener()
     }
 
     private fun onError() {
